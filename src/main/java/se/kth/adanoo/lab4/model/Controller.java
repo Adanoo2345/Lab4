59a996a0fb451c0d35cb153eb158c5ab5e9fe8fb
Omara <omaradan86@gmail.com>
2025-11-19 02:36:00 +0100

    View och Controller TIllagda

---
+++ src/main/java/se/kth/adanoo/lab4/model/Controller.java
@@ -0,0 +1,81 @@
+package se.kth.adanoo.lab4.model;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import se.kth.adanoo.lab4.util.FileIO;
+import se.kth.adanoo.lab4.util.ImagePixelsConverter;
+
+import java.io.File;
+
+public class Controller {
+
+    private final Stage stage;
+    private final ImageModel model;
+    private final View view;
+
+    public Controller(Stage stage, ImageModel model, View view) {
+        this.stage = stage;
+        this.model = model;
+        this.view = view;
+    }
+
+    public void onLoadImage() {
+        try {
+            FileChooser fc = new FileChooser();
+            fc.setTitle("Open Image");
+            fc.getExtensionFilters().add(
+                    new FileChooser.ExtensionFilter("Image files",
+                            "*.png", "*.jpg", "*.jpeg", "*.bmp")
+            );
+
+            File file = fc.showOpenDialog(stage);
+            if (file == null) return;
+
+            Image img = FileIO.loadImage(file);
+            int[][] px = ImagePixelsConverter.imageToPixels(img);
+
+            model.setOriginalAndCurrent(px);
+            view.displayImage(img);
+
+        } catch (Exception e) {
+            showError("Could not load image\n" + e.getMessage());
+        }
+    }
+
+    public void onSaveImage() {
+        try {
+            FileChooser fc = new FileChooser();
+            fc.setTitle("Save Image");
+
+            File file = fc.showSaveDialog(stage);
+            if (file == null) return;
+
+            Image img = ImagePixelsConverter.pixelsToImage(model.getCurrentPixels());
+            FileIO.saveImage(img, file);
+
+        } catch (Exception e) {
+            showError("Could not save image\n" + e.getMessage());
+        }
+    }
+
+    public void onRevert() {
+        model.setOriginalAndCurrent(model.getCurrentPixels());
+        updateView();
+    }
+
+    public void onGrayScale() { updateView(); }
+    public void onBlur()      { updateView(); }
+    public void onSharpen()   { updateView(); }
+
+    private void updateView() {
+        Image img = ImagePixelsConverter.pixelsToImage(model.getCurrentPixels());
+        view.displayImage(img);
+    }
+
+    private void showError(String msg) {
+        new Alert(Alert.AlertType.ERROR, msg, ButtonType.OK).show();
+    }
+}
---
+++ src/main/java/se/kth/adanoo/lab4/model/View.java
@@ -0,0 +1,65 @@
+package se.kth.adanoo.lab4.model;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class View {
+
+    private Controller controller;
+    private final Stage stage;
+
+    private final ImageView imageView = new ImageView();
+
+    private final MenuItem loadItem = new MenuItem("Load Image");
+    private final MenuItem saveItem = new MenuItem("Save Image");
+    private final MenuItem revertItem = new MenuItem("Revert");
+
+    private final MenuItem grayItem = new MenuItem("Grayscale");
+    private final MenuItem blurItem = new MenuItem("Blur");
+    private final MenuItem sharpenItem = new MenuItem("Sharpen");
+
+    public View(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+        connectActions();
+    }
+
+    private void connectActions() {
+        loadItem.setOnAction(e -> controller.onLoadImage());
+        saveItem.setOnAction(e -> controller.onSaveImage());
+        revertItem.setOnAction(e -> controller.onRevert());
+
+        grayItem.setOnAction(e -> controller.onGrayScale());
+        blurItem.setOnAction(e -> controller.onBlur());
+        sharpenItem.setOnAction(e -> controller.onSharpen());
+    }
+
+    public void show() {
+        BorderPane root = new BorderPane();
+
+        Menu fileMenu = new Menu("File", null, loadItem, saveItem, revertItem);
+        Menu processMenu = new Menu("Process", null, grayItem, blurItem, sharpenItem);
+
+        MenuBar menuBar = new MenuBar(fileMenu, processMenu);
+
+        root.setTop(menuBar);
+        root.setCenter(imageView);
+
+        stage.setScene(new Scene(root, 900, 600));
+        stage.setTitle("Lab 4");
+        stage.show();
+    }
+
+    public void displayImage(Image img) {
+        imageView.setImage(img);
+    }
+}
